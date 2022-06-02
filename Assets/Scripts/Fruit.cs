573ee366ad832af001d88c5a7d657bc3f9afa000
John Haste <joaovictorspteixeira@gmail.com>
2022-06-02 17:37:20 -0300

    Created Component DestroyIfNotMoving

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -5,32 +5,13 @@ using EzySlice;
 
 public class Fruit : MonoBehaviour
 {
-    public Vector3 lastPosition;
     public Material insideMaterial;
     public GameObject splashParticleFruit;
-
-    void Update()
-    {
-        StartCoroutine(CheckIfMoved());
-    }
-
-    private IEnumerator CheckIfMoved()
-    {
-        while (true)
-        {
-           if(lastPosition == transform.position){
-               this.gameObject.AddComponent<DestroyAfterSeconds>();
-           }
-           lastPosition = transform.position;
-           yield return new WaitForSeconds(5f);
-        }
-    }
 
     public void EmitSplash()
     {
         if(splashParticleFruit != null)
         {
-            print("Splash");
             GameObject splashParticle = Instantiate(splashParticleFruit, transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
             //splashParticle.transform.SetParent(transform);
         }
